de4592aada2c3a4f925c3f2a453c19701d5ddc24
rolandwalet <33226852+rolandwalet@users.noreply.github.com>
2018-04-27 15:53:10 +0100

    Add files via upload

--- Items.java
+++ Items.java
@@ -10,10 +10,28 @@ public class Items {
 	public Items() {
 		treasureLocation = makePosition(10);									//set locations
 		keyLocation = makePosition(5);
-		mapLocation = makePosition(1);
+		while (keyLocation == treasureLocation) {
+			keyLocation = makePosition(5);
+		}
+		mapLocation = makePosition(2);
+		while (mapLocation == treasureLocation || mapLocation == keyLocation) {
+			mapLocation = makePosition(2);
+		}
 		gogglesLocation = makePosition(5);
+		while (gogglesLocation == treasureLocation || gogglesLocation == keyLocation || gogglesLocation == mapLocation) {
+			gogglesLocation = makePosition(5);
+		}
 		sign1Location = makePosition(3);
+		while (sign1Location == treasureLocation || sign1Location == keyLocation || sign1Location == mapLocation
+				|| sign1Location == gogglesLocation) {
+			sign1Location = makePosition(3);
+		}
 		sign2Location = makePosition(3);
+		
+		while (sign2Location == treasureLocation || sign2Location == keyLocation || sign2Location == mapLocation
+				|| sign2Location == gogglesLocation || sign2Location == sign1Location) {
+			sign2Location = makePosition(3);
+		}
 	}
 	
 	//random position creator
